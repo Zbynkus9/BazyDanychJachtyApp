bc3f18bdb9101e53709e7bc11eaa55ca07296a62
Zbynkus9 <kamil.pojedynek@gmail.com>
2025-12-02 21:09:06 +0100

    Stop tracking build directory

--- build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_BazyDanychJachtyApp_FILE D:/Projects/QtProjects/BazyDanychJachtyApp/BazyDanychJachtyApp/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/BazyDanychJachtyApp.exe)
-set(__QT_DEPLOY_TARGET_BazyDanychJachtyApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_BazyDanychJachtyApp_RUNTIME_DLLS D:/Qt/6.10.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.10.0/mingw_64/bin/Qt6Sql.dll;D:/Qt/6.10.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.10.0/mingw_64/bin/Qt6Core.dll)
