e5ec0a3b42d8154e2c848879564a444acc3af375
Zbynkus9 <kamil.pojedynek@gmail.com>
2026-01-17 19:03:33 +0100

    Implemented Yacht selection window

--- selectyachtwindow.cpp
+++ selectyachtwindow.cpp
@@ -11,7 +11,7 @@ SelectYachtWindow::SelectYachtWindow(QWidget *parent, int userId, QSqlDatabase d
     ui->setupUi(this);
 
     QSqlQuery yachts(m_db);
-    yachts.prepare("SELECT yachts.id_yacht AS 'yId', yachts.name AS 'yName' FROM yacht JOIN yachts_ownership ON yachts.id_yacht = yacht_ownership.yacht_id WHERE owner_id = :currentUserId ORDER BY yachts.name");
+    yachts.prepare("SELECT yachts.id_yacht AS 'yId', yachts.name AS 'yName' FROM yachts JOIN yacht_ownership ON yachts.id_yacht = yacht_ownership.yacht_id WHERE owner_id = :currentUserId AND NOT EXISTS ( SELECT 1 FROM yacht_ownership history WHERE history.owner_id = users.id_user AND history.yacht_id = :yId AND history.ownership_flag = 'Past') ORDER BY yachts.name");
     yachts.bindValue(":currentUserId", m_currentUserId);
 
     if (!yachts.exec()) {
@@ -33,7 +33,14 @@ SelectYachtWindow::~SelectYachtWindow()
 
 void SelectYachtWindow::on_buttonBox_accepted()
 {
+    int selectedYacht = ui->YachtComboBox->itemData(ui->YachtComboBox->currentIndex()).toInt();
     // open RemoveCoOwnerWindow QDialog Widget and pass selected Yacht Id
+    // 1. Create the instance (Stack allocation)
+    // Pass 'this' as parent so it centers over the Main Window
+    RemoveCoOwnerWindow dialog(this, m_currentUserId, selectedYacht, m_db);
+
+    // 2. Launch it effectively "freezing" the code here until the dialog closes
+    dialog.exec();
 }
 
 
