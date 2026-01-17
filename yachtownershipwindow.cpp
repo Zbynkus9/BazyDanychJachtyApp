89c157985065daa224a18e06175ace6e13d8b9d2
Zbynkus9 <kamil.pojedynek@gmail.com>
2026-01-17 19:06:06 +0100

    Update yachtownershipwindow.cpp

--- yachtownershipwindow.cpp
+++ yachtownershipwindow.cpp
@@ -1,4 +1,7 @@
 #include "yachtownershipwindow.h"
+#include "addcoownerwindow.h"
+#include "changeownerwindow.h"
+#include "selectyachtwindow.h"
 #include "ui_yachtownershipwindow.h"
 
 YachtOwnershipWindow::YachtOwnershipWindow(QWidget *parent, int userId, QSqlDatabase db)
@@ -47,6 +50,12 @@ void YachtOwnershipWindow::on_AddOwnerBTN_clicked()
     /*
         Otwiera QDialog z wyborem jachtów których jesteśmy obecnym właścicielem oraz wpisaniem Username który jest sprawdzany czy istnieje i czy już nie jest obecnym właścicielem a następnie dodaje odpowiedni rekord właściciela
     */
+    // 1. Create the instance (Stack allocation)
+    // Pass 'this' as parent so it centers over the Main Window
+    AddCoOwnerWindow dialog(this, m_currentUserId, m_db);
+
+    // 2. Launch it effectively "freezing" the code here until the dialog closes
+    dialog.exec();
 }
 
 
@@ -55,6 +64,12 @@ void YachtOwnershipWindow::on_DeleteOwnerBTN_clicked()
     /*
         Otwiera QDialog z wyborem jachtów których jesteśmy obecnym właścicielem oraz z wyborem pasujących innych właścicieli wybranego jachtu (poza głóWnym właścicielem) i potwierdzeniem i anulacją usunięcia (aka zmiany na przeszłego właściciela)
     */
+    // 1. Create the instance (Stack allocation)
+    // Pass 'this' as parent so it centers over the Main Window
+    SelectYachtWindow dialog(this, m_currentUserId, m_db);
+
+    // 2. Launch it effectively "freezing" the code here until the dialog closes
+    dialog.exec();
 }
 
 
@@ -64,5 +79,10 @@ void YachtOwnershipWindow::on_ChangeOwnerBTN_clicked()
         Otwiera QDialog z wyborem jachtów których jesteśmy obecnym głównym właścicielem oraz wpisaniem Username który jest sprawdzany czy istnieje.
         Po potwierdzeniu i poprawności Username, aktualizowani są wszyscy właściciele razem z głównym wybranego jachtu na przeszłych właścicieli i wpisany Username jest ustawiany jako główny właściciel
     */
-}
+    // 1. Create the instance (Stack allocation)
+    // Pass 'this' as parent so it centers over the Main Window
+    ChangeOwnerWindow dialog(this, m_currentUserId, m_db);
 
+    // 2. Launch it effectively "freezing" the code here until the dialog closes
+    dialog.exec();
+}
